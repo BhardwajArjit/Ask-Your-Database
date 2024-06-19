016dca45f5fc9e6f0d1ab8a3c1f39f0fde848508
BhardwajArjit <iamarjitbhardwaj@gmail.com>
2024-06-20 00:22:30 +0530

    update homepage

--- Homepage.py
+++ Homepage.py
@@ -1,10 +1,15 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Project M",
-    page_icon="👋",
-)
 
-st.title("Stay tuned...")
-st.sidebar.success("Select a page above.")
+def main():
+    st.set_page_config(
+        page_title="Project M",
+        page_icon="👋",
+    )
 
+    st.title("Stay tuned...")
+    st.sidebar.success("Select a page above.")
+
+
+if __name__ == '__main__':
+    main()
